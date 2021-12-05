f3108b98539fa9bac3779d587ceb66452b37d63c
Omama Bashir <86739984+Omama1234@users.noreply.github.com>
2021-12-05 22:14:07 +0530

    Create WrapperClass.java

---
+++ WrapperClass.java
@@ -0,0 +1,23 @@
+
+package Test;
+
+
+
+
+class WrapperClass{
+
+	public static void main(String [] args)
+	{
+		
+		Integer i1=Integer.valueOf("101",2);
+		System.out.println(i1.intValue());
+		
+		Float i =Float.parseFloat("12.45");
+		System.out.println(i);
+		
+		
+	
+	}
+	
+
+}

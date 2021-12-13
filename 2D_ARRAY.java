152a7c81481426085575ed666aa5e1c487a2607c
Omama Bashir <86739984+Omama1234@users.noreply.github.com>
2021-12-13 22:31:52 +0530

    Create 2D_ARRAY.java

---
+++ 2D_ARRAY.java
@@ -0,0 +1,19 @@
+package Test;
+import java.io.*;
+import java.util.*;
+
+
+public class 2D_ARRAY {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		//Scanner sc=new Scanner(System.in);
+		int[][] arr=new int[4][];
+		
+		System.out.println("arr[0]="+arr[0]);
+		
+		
+
+	}
+
+}

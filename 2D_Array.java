a4e1c845024be5b3ee94a28ace9ff3e4da06cab7
Omama Bashir <86739984+Omama1234@users.noreply.github.com>
2021-12-13 22:30:40 +0530

    Create 2D_Array.java

---
+++ 2D_Array.java
@@ -0,0 +1,22 @@
+package Test;
+import java.io.*;
+import java.util.*;
+
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Scanner sc=new Scanner(System.in);
+		int[][] arr=new int[4][];
+		
+		System.out.println("arr[0][0]="+arr[0][0]);  //error
+		
+		
+
+	}
+
+}
+
+Output -> Exception in thread "main" java.lang.NullPointerException: Cannot load from int array because "arr[0]" is null
+	at HelloWorld/Test.Array.main(Array.java:13)

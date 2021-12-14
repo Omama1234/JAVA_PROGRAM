0d5900629d744832f08855356288140280bbba81
Omama Bashir <86739984+Omama1234@users.noreply.github.com>
2021-12-14 20:29:05 +0530

    Create String_methods.java

---
+++ String_methods.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    String s1=new String("Computer");
+	    System.out.println(s1);
+	    s1.toUpperCase();
+	    System.out.println(s1.toUpperCase());
+	    s1.toLowerCase();
+	     System.out.println(s1.toLowerCase());
+	}
+}

0a3fac230a333e3542217f96d50b44025ee8273c
Omama Bashir <86739984+Omama1234@users.noreply.github.com>
2021-12-14 23:14:07 +0530

    Update IndexOf.java

--- IndexOf.java
+++ IndexOf.java
@@ -19,3 +19,26 @@ public class IndexOf {
 
 OUTPUT ->
 2
+
+	
+	
+package flex;
+
+import java.lang.*;
+
+public class IndexOf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s=new String("Commuter");
+		System.out.println(s.indexOf('m',3));
+		
+		
+
+	}
+
+}
+
+OUTPUT ->
+-1

6266b66f69bc7721cffd7f8cc415f4e046d7d3c0
Omama Bashir <86739984+Omama1234@users.noreply.github.com>
2021-12-14 23:01:07 +0530

    Create IndexOf.java

---
+++ IndexOf.java
@@ -0,0 +1,21 @@
+package flex;
+
+import java.lang.*;
+
+public class IndexOf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s=new String("Computer");
+		System.out.println(s.indexOf('m'));
+		
+		
+
+	}
+
+}
+
+
+OUTPUT ->
+2

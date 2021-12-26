3b76a6d8c1d0ed569d0a96a6fdb77d8d33a2fb82
Omama Bashir <86739984+Omama1234@users.noreply.github.com>
2021-12-26 15:40:17 +0530

    Create TwoDimensionalArray.java

---
+++ TwoDimensionalArray.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class TwoDimensionalArray
+{
+	public static void main(String[] args) {
+		int[][] arr1=new int[][]{{2,4,5,6},{5,8,2,6}}; //No error 
+		System.out.println(arr1[0].length);
+	//	int[][] arr2=new int [][];//Error
+	}
+}
+
+OUTPUT ->
+4

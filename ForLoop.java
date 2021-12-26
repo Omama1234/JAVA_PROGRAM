f32224e11bce17d6e27f45fbfed6a20508ab87f4
Omama Bashir <86739984+Omama1234@users.noreply.github.com>
2021-12-26 19:57:45 +0530

    Create ForLoop.java

---
+++ ForLoop.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class ForLoop
+{
+    
+	public static void main(String[] args) {
+	   int a=1;
+	   for(System.out.print("Omama");a<5;a++)
+	   {
+	       System.out.print(a);
+	   }
+	    
+	}
+}
+OUTPUT -> Omama1234

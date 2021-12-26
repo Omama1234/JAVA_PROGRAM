eb52827368bba3afff9641fb4ed222772633d5f9
Omama Bashir <86739984+Omama1234@users.noreply.github.com>
2021-12-26 15:45:02 +0530

    Create Basics.java

---
+++ Basics.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Basics
+{
+    public static void main(int[] args)  //No error
+    {
+        System.out.println("Bashir");  //This will not be printed to the output
+    }
+	public static void main(String[] args) {
+		System.out.println("Omama"); // This will be printed to the output 
+	}
+}
+
+OUTPUT ->
+Omama

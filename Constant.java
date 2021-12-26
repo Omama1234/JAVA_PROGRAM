b2cdb252bd00b37e155a96ca3589a12e2961e9f4
Omama Bashir <86739984+Omama1234@users.noreply.github.com>
2021-12-26 15:23:55 +0530

    Create Constant.java

---
+++ Constant.java
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Constant
+{
+	public static void main(String[] args) {
+		int a=1_23_4;
+		System.out.println(++a);
+	}
+}
+OUTPUT ->
+1235
